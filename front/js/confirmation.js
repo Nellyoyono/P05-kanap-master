1920fabe84241519e59ac651439e6a9de1a026ab
Nelo <101742968+Nellyoyono@users.noreply.github.com>
2022-08-26 18:27:41 +0200

    Update confirmation.js

--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -1,11 +1,10 @@
-/*-----------------------------------------------------
-                AFFICHAGE PRODUITS
-------------------------------------------------------*/
+//-affichage produits-//-
 
-/* récupération du numéro de commande dans l'url de la page */
+
+//récupération du numéro de commande dans l'url de la page //
 const url = new URL(window.location.href);
 const id = url.searchParams.get("id");
-/* affichage du numéro de commande */
+//affichage du numéro de commande //
 const orderId = document.getElementById("orderId");
 orderId.textContent = id;
 localStorage.clear();
