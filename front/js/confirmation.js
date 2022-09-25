f17ad2a589f3d93179ffbc117b33dd0f30c3d1c4
Nelo <101742968+Nellyoyono@users.noreply.github.com>
2022-09-25 05:04:30 +0200

    Update confirmation.js

--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -1,9 +1,8 @@
 function showNoCommand() {
-/* récupération du numéro de commande dans l'url de la page */
-let params = new URL(document.location).searchParams;
+let params = new URL(document.location).searchParams;//récupération du numéro de commande dans l'url de la page
 let orderId = params.get("orderId");
 
-/* affichage du numéro de commande */
+//affichage du numéro de commande 
 document.getElementById("orderId").textContent = orderId;
 localStorage.clear();
 }
